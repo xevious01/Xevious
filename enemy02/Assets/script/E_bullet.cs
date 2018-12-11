d4da7f8d0db97810e516211cfc36498ed84c3081
DA\nwuser <yokoyama.yda@gmail.com>
2018-12-11 09:27:04 +0900

    横山担当

---
+++ enemy02/Assets/script/E_bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Rigidbody2D>().velocity
+             = new Vector2(0, 3.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Animator anim = GetComponent<Animator>();
+        AnimatorStateInfo animInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+
+        if (animInfo.normalizedTime > 3.0f)
+        {
+            Destroy(gameObject);    //自分自身を消去する
+        }
+
+    }
+}
